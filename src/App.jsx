1e51ab2f5f8dd5586d4aebfac3d0d06437b42d81
Charlotte Spencer <charlottelaspencer@gmail.com>
2017-12-03 19:50:49 +0000

    feat: hello, world.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+import Header from "./components/header"
+
+class App extends Component {
+  render() {
+    return <Header />
+  }
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "../styles/header.css"
+
+const Header = () => {
+  return (
+    <header className="header bg-washed-blue">
+      <h1 className="f1">toota</h1>
+      <h2 className="f2">let's toot</h2>
+    </header>
+  )
+}
+
+export default Header
